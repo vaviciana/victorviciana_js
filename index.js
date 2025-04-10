49f1ec736e10edd3ae80b204cfb0df2601574191
victor.viciana <vaviciana@gmail.com>
2025-04-10 17:23:17 +0200

    inicio git

---
+++ index.js
@@ -0,0 +1,19 @@
+const diasNoDisponibles = [5, 6, 12, 13, 19, 20, 26, 27, 17, 18, 23];
+let diaSolicitado;
+
+while (true) {
+    diaSolicitado = parseInt(prompt('¿Qué día te gustaría reservar una sesión gratuita?'));
+
+    if (diaSolicitado < 1 || diaSolicitado > 31) {
+        alert("Por favor, introduce un número del 1 al 31.");
+        continue;
+    }
+
+    if (!diasNoDisponibles.includes(diaSolicitado)) {
+        console.log('Reservado');
+        diasNoDisponibles.push(diaSolicitado);
+        break; 
+    } else {
+        alert("Lo sentimos, esa fecha no está disponible. Prueba otra.");
+    }
+}
